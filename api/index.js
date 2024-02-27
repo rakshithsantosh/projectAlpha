fa12b71f107b909445eac8943bcd233d8031a4ba
rakshith_s <rakshithsantosh@gmail.com>
2024-02-27 10:08:19 +0530

    created user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,6 +16,8 @@ app.use(express.json());
   
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
+
+app.use(cookieParser())
 
 app.use((err,req,res,next) =>{
     const statusCode = err.statusCode || 500;
