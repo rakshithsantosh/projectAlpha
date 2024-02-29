8ffe65052ad31323381d1936b64b4d77853f8221
rakshith_s <rakshithsantosh@gmail.com>
2024-02-29 09:00:22 +0530

    complete create listing Ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import {getDownloadURL, getStorage,ref, uploadBytesResumable} from 'firebase/sto
 import { app } from '../firebase'
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutFailure, signOutStart, signOutSuccess, updateUserFailure,updateUserStart,updateUserSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
-
+import {Link} from 'react-router-dom'
 
 export default function Profile() {
   const dispatch = useDispatch()
@@ -114,6 +114,7 @@ export default function Profile() {
         <input type="email" defaultValue={currentUser.email} placeholder="email" id="email"className="border p-3 rounded-lg" onChange={handleChange}/>
         <input type="password" placeholder="password" id="password"className="border p-3 rounded-lg" onChange={handleChange}/>
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">update</button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={'/create-listing'}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
